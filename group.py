db5c88f538ed85a40440428b072dbe80022023ac
Wei Miao <ucapwmi@ucl.ac.uk>
2024-10-14 10:34:25 +0100

    Part1_my_group

--- group.py
+++ group.py
@@ -2,4 +2,26 @@
 
 # Your code to go here...
 
-my_group =
+my_group = {
+    'Jill' : {
+        'age' : 26,
+        'job' : 'biologist',
+        'connection' : {'Zalika' : 'friend', 'John' : 'partner'}
+    },
+    'Zalika' : {
+        'age' : 28,
+        'job' : 'artist',
+        'connection' : {'Jill' : 'friend'}
+    },
+    'John' : {
+        'age' : 27,
+        'job' : 'writer',
+        'connection' : {'Jill' : 'partner'}
+    },
+    'Nash' : {
+        'age' : 34,
+        'job' : 'chef',
+        'connection' : {'John' : 'cousin', 'Zalika' : 'landlord'}
+    }
+}
+print(my_group)
