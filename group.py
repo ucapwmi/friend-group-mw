2b6227fc0835d5fc91ee16d4fed2c5b48da3c3eb
comp0233 <ucapmox@ucl.ac.uk>
2024-10-14 18:15:09 +0800

    First_com

--- group.py
+++ group.py
@@ -2,4 +2,25 @@
 
 # Your code to go here...
 
-my_group =
+class Person:
+    def __init__(self,name,age,work):
+        self.name=name
+        self.age=age
+        self.work=work
+        self.relation=[]
+
+    def add_relationship(self,name,relationship):
+        self.relation.append({"Name":name,"Relationship":relationship})
+
+#if _name_ == "_main_":
+
+Jill=Person("Jill",26,"biologist")
+Zalika=Person("Zalika",  28, "artis")
+John=Person("John", 27, "writer")
+Nash=Person("Nash", 34, "chef")
+
+Jill.add_relationship("Zalika","friend")
+Jill.add_relationship("John","partner")
+Zalika.add_relationship("Jill","friend")
+print(Jill.relation)
+        
